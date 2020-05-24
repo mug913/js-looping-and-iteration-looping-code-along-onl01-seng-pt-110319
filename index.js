d86dedcc76c023692dc627c2a41cd1ff6e445536
Mug <brad.alexander913@gmail.com>
2020-05-24 15:33:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+let cards = [];
+
+function writeCards(list, event) {
+    for (let i = 0; i < list.length; i++) {
+        cards.push(`Thank you, ${list[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards
+
+}
+
+
+
+
+function countDown(number) {
+    let i = number;
+    while (i > -1){
+        console.log(i--);
+    }
+}
